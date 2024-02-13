596d295c9240955e28074f39ad1d0370c5916a84
cvk123 <pavelvintr11@seznam.cz>
2024-02-13 07:15:18 +0100

    insert_data

--- main.py
+++ main.py
@@ -18,6 +18,22 @@ def create_table():
               AGE INT,
               ADDRESS TEXT
   )''')
-
   conn.commit()
   conn.close()
+
+# Insert data into the table
+
+def insert_data():
+  conn = psycopg2.connect(
+      dbname='student',
+      user='postgres',
+      password='admin123',
+      host='localhost',
+      port='5432'
+    )
+
+  cur = conn.cursor()
+  cur.execute("INSERT INTO teacher (NAME, AGE, ADDRESS) VALUES (%s, %s, %s)", ('Dave', 29, 'Canada'))
+  conn.commit()
+  conn.close()
+  
