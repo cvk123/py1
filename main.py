2fa725493d7272de394f66a87a1501e19796c63e
cvk123 <pavelvintr11@seznam.cz>
2024-02-13 07:01:24 +0100

    create_table

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import psycopg2
 
 # Connect to the database
+
+def create_table():
+  conn = psycopg2.connect(
+    dbname='student',
+    user='postgres',
+    password='admin123',
+    host='localhost',
+    port='5432'
+  ) 
+
+  cur = conn.cursor()
+  cur.execute('''CREATE TABLE teacher(
+              ID SERIAL,
+              NAME TEXT,
+              AGE INT,
+              ADDRESS TEXT
+  )''')
+
+  conn.commit()
+  conn.close()
