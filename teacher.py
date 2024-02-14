ebfd0895c99bdb18e5146e8d4f4de04b1e6e6b9c
cvk123 <pavelvintr11@seznam.cz>
2024-02-14 11:19:28 +0100

    finish age section

--- teacher.py
+++ teacher.py
@@ -17,4 +17,18 @@ label_name.grid(row=1, column=0)
 entry_name = Entry(root, font=("Arial", 15))
 entry_name.grid(row=1, column=1)
 
+## age sections
+label_age = Label(root, text="Age: ", font=("Arial", 20), bg="#121212", fg="white")
+label_age.grid(row=2, column=0)
+
+entry_age = Entry(root, font=("Arial", 15))
+entry_age.grid(row=2, column=1)
+
+## address sections
+label_address = Label(root, text="Address: ", font=("Arial", 20), bg="#121212", fg="white")
+label_address.grid(row=3, column=0)
+
+entry_address = Entry(root, font=("Arial", 15))
+entry_address.grid(row=3, column=1)
+
 root.mainloop()
